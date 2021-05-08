d88f50e3217f4000845ff6d23e024293e4756bb6
kirti18patel <kirti18patel@yahoo.com>
2021-05-07 21:41:57 -0400

    questions displayed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,62 @@
 var questionBank= [
     {
         question : "Which built-in method combines the text of two strings and returns a new string?",
-        options : ["A - append()", "B - concat()" , "C - attach()", "D - None of the above."],
-        answer : "B - concat()"
+        options : ["1. append()", "2. concat()" , "3. attach()", "4. None of the above."],
+        answer : "2. concat()"
     },
     {
         question : "Which of the following function of String object causes a string to be displayed as a subscript, as if it were in a <sub> tag?",
-        options : ["A - sup()", "B - small()", "C - strike()", "D - sub()"],
-        answer : "D - sub()"
+        options : ["1. sup()", "2. small()", "3. strike()", "4. sub()"],
+        answer : "4. sub()"
     },
     {
         question : "Which of the following function of Array object reverses the order of the elements of an array?",
-        options : ["A - reverse()","B - push()", "C - reduce()", "D - reduceRight()"],
-        answer : "A - reverse()"
+        options : ["1. reverse()","2. push()", "3. reduce()", "4. reduceRight()"],
+        answer : "1. reverse()"
     },
     {
         question : "Which built-in method returns the length of the string?",
-        options : ["A - length()", "B - size()", "C - index()", "D - None of the above."],
-        answer : "A - length()"
+        options : ["1. length()", "2. size()", "3. index()", "4. None of the above."],
+        answer : "1. length()"
     },
     {
         question : "How can you get the type of arguments passed to a function?",
-        options : ["A - using typeof operator", "B - using getType function", "C - Both of the above", "D - None of the above."],
-        answer : "A - using typeof operator"
+        options : ["1. using typeof operator", "2. using getType function", "3. Both of the above", "4. None of the above."],
+        answer : "1. using typeof operator"
     },
     {
         question : "Which of the following function of String object causes a string to be displayed as struck-out text, as if it were in a <strike> tag?",
-        options : ["A - sup()", "B - small()", "C - strike()", "D - sub()"],
-        answer : "C - strike()"
+        options : ["1. sup()", "2. small()", "3. strike()", "4. sub()"],
+        answer : "3. strike()"
     }
 ];
 
 var button = document.querySelector(".start-btn");
 var headingBox = document.querySelector(".heading-box");
 var questionsBox = document.querySelector(".questionsBox");
+var quesRemove = document.querySelector(".question");
 
-var displayQuestions = function(){
+console.log(questionBank.length);
+var displayQuestions = function(){    
     headingBox.remove();
-    var questions = document.createElement("h1");
-    questions.textContent = questionBank[0].question;
-    questions.className = "question";
-    questionsBox.appendChild(questions);
+    for(var i=0; i<questionBank.length;i++){
+        // quesRemove.remove();
+        var questions = document.createElement("h1");
+        questions.textContent = questionBank[i].question;
+        questions.className = "question";
+        questionsBox.appendChild(questions);
 
-    var options = document.createElement("li");
-    options.textContent = questionBank[0].options;
-    options.className = "options-list";
-    questionsBox.appendChild(options);
+        var optionsList = document.createElement("ul");
+        optionsList.className = "options-list";
+
+            for(var j=0; j<questionBank[i].options.length; j++){
+                var options = document.createElement("li");
+                options.textContent = questionBank[i].options[j];
+                options.className = "options";
+                optionsList.appendChild(options);
+            }
+        questionsBox.appendChild(optionsList);
+    }
 };
 
 button.addEventListener("click", displayQuestions);
